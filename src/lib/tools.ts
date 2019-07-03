bd297e85102fbef0ed6119f64b748f7da6ca5f57
SchumyHao <schumyhaojl@126.com>
2019-07-04 00:41:41 +0800

    Remove unused function
    Signed-off-by: SchumyHao <schumyhaojl@126.com>

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 /**
  * Tests whether the given variable is a real object and not an Array
  * @param it The variable to test
@@ -19,26 +17,6 @@ export function isObject(it: any): it is object {
 export function isArray(it: any): it is any[] {
     if (Array.isArray != null) return Array.isArray(it);
     return Object.prototype.toString.call(it) === "[object Array]";
-}
-
-/**
- * Translates text using the Google Translate API
- * @param text The text to translate
- * @param targetLang The target languate
- */
-export async function translateText(text: string, targetLang: string): Promise<string> {
-    if (targetLang === "en") return text;
-    try {
-        const url = `http://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}&ie=UTF-8&oe=UTF-8`;
-        const response = await axios({url, timeout: 5000});
-        if (isArray(response.data)) {
-            // we got a valid response
-            return response.data[0][0][0];
-        }
-        throw new Error("Invalid response for translate request");
-    } catch (e) {
-        throw new Error(`Could not translate to "${targetLang}": ${e}`);
-    }
 }
 
 export function RGBToHex(rgb: number[]){
