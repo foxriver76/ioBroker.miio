6469488f4bd7d58b37546621b1e0c2bd91239342
SchumyHao <schumyhaojl@126.com>
2019-09-13 22:40:37 +0800

    update test
    Signed-off-by: SchumyHao <schumyhaojl@126.com>

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,38 @@
 const path = require("path");
-const { tests } = require("@iobroker/testing");
+const { tests, utils } = require("@iobroker/testing");
+const miioLiteMock = require("./lib/miio-lite-mock");
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
+console.log("run miio unit test");
+tests.unit(path.join(__dirname, ".."), {
+    additionalMockedModules: {
+        "miio-lite": miioLiteMock,
+        // Use the {CONTROLLER_DIR} placeholder to access the path where JS-Controller would be installed.
+        // Don't forget to provide mocks for every module you need, as they don't exist in unit tests
+        //"{CONTROLLER_DIR}/lib/tools.js": {},
+    },
+    overwriteAdapterConfig(config) {
+        config.autoDiscover = true;
+        // Don't forget to return it
+        return config;
+    },
+    defineAdditionalTests() {
+        // Create mocks and asserts
+        const { adapter, database } = utils.unit.createMocks();
+        const { assertObjectExists } = utils.unit.createAsserts(database, adapter);
+
+        describe("my test", () => {
+
+            afterEach(() => {
+                // The mocks keep track of all method invocations - reset them after each single test
+                adapter.resetMockHistory();
+                // We want to start each test with a fresh database
+                database.clear();
+            });
+
+            it("works", () => {
+                console.log(`${JSON.stringify(adapter.objects)}`)
+            });
+        });
+    }
+});
